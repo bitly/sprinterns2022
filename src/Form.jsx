b8842013944ccfaedba68e92fc2b72525870120a
Kathy Lavoie <kathy.lavoie@bit.ly>
2020-12-28 11:35:48 -0700

    more cleanup

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,13 +3,12 @@ import KVInputs from "./KVInputs.jsx";
 import axios from 'axios';
 import ReactJson from 'react-json-view'
 
-const Form = () => {
+const Form = ({method, endpoint}) => {
   const { useState } = React;
-  const end = '';
-  const formV = '';
+
   const [response, setResponse] = useState({});
   const [authToken, setAuthToken] = useState('');
-  const [apiEndpoint, setApiEndpoint] = useState(end);
+  const [apiEndpoint, setApiEndpoint] = useState(endpoint);
   const [params, setParams] = useState([{key:'', value:''}]);
 
   const handleKeyValueChange = (event, type, idx) => {  
@@ -29,186 +28,60 @@ const Form = () => {
   }
   
   const addKeyVal = () => {
-    console.log("WE CLICKED THE ADD BUTTON") 
     setParams([...params, {key:"", value:""}]);
   }
 
   const handleSubmit = () => {
-    console.log('BUTTON CLICKED')
     const SERVER_URL = "https://5000-f9e7f7e4-c57e-43fb-8340-f9648942ba09.ws-us02.gitpod.io/"
-    console.log(SERVER_URL + '/api')
     const arrayToObject = (array) =>
-        array.reduce((obj, item) => {
-            if(item.key) {
-                obj[item.key] = item.value
-            }
-            return obj
-    }, {})
-  
+      array.reduce((obj, item) => {
+        if(item.key) {
+            obj[item.key] = item.value
+        }
+        return obj
+    }, {});
     const keyvalObject = arrayToObject(params)
-    console.log(keyvalObject) 
-        axios
-            .post(SERVER_URL + 'api', {apiEndpoint: apiEndpoint, authToken: authToken, params: keyvalObject, Method:this.props.Method})
-            .then((res) => {   
-                console.log(res.data)
-                setResponse(res.data)
-            })   
-    }  
+    axios
+      .post(SERVER_URL + 'api', {apiEndpoint: apiEndpoint, authToken: authToken, params: keyvalObject, Method: method})
+      .then((res) => {   
+        setResponse(res.data)
+      })   
+  }  
     
-    const handleReset = () => {
-      setAuthToken('');
-      setApiEndpoint('');
-      setParams([{key:'', value:''}]);
-    }
+  const handleReset = () => {
+    setAuthToken('');
+    setApiEndpoint('');
+    setParams([{key:'', value:''}]);
+  }
 
   return (
     <div className="inputForms">
       <center>
         <label>API Endpoint</label><br />
-            <input className ="endpointBox" value={formV}
-            type="text" name='apiEndpoint'onChange={(e) => {
-              console.log('E', e.target.value);
-              setApiEndpoint(e.value)}
-            } 
-            /> <br /> <br />
-        <label>Authorization Token</label><br />
-            <input className ="authTokenBox" value={authToken}
-            type="text" name='authToken' onChange={setAuthToken} 
-        /> <br /> <br />
-      
+        <input className ="endpointBox" value={apiEndpoint}
+        type="text" name='apiEndpoint' onChange={(event) => setApiEndpoint(event.target.value)}
+        /> 
+        <br /> 
+        <br />
+        <label>Authorization Token</label>
+        <br />
+        <input className ="authTokenBox" value={authToken} type="text" name='authToken' onChange={(event) => setAuthToken(event.target.value)} /> 
+        <br /> 
+        <br />
         <KVInputs handleChange={handleKeyValueChange} params={params}/>
-          <button className = 'add' onClick={addKeyVal}>Add+</button>
-          </center>
-          <div className="resultsBox"> 
-            <center>
-              <h2>Results</h2>
-            </center> 
-            <ReactJson src={response} theme="bright:inverted"/>
-        </div>   
-            <br /> <button className="submit" onClick={handleSubmit}>Submit</button>
-            <button className="resetbutton" onClick={handleReset}>Reset</button> 
+        <button className = 'add' onClick={addKeyVal}>Add+</button>
+      </center>
+      <div className="resultsBox"> 
+        <center>
+          <h2>Results</h2>
+        </center> 
+        <ReactJson src={response} theme="bright:inverted"/>
+      </div>   
+      <br /> 
+      <button className="submit" onClick={handleSubmit}>Submit</button>
+      <button className="resetbutton" onClick={handleReset}>Reset</button> 
     </div>
   );
 }
-
-// export default class Form extends React.Component {
-//  constructor(props) {
-//     super(props);
-//     this.state = {
-//         response: {},
-//         authToken: '',
-//         apiEndpoint: this.props.End,
-//         params: [{key:"", value:""}],
-        
-//     };    
-
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//     this.handleReset = this.handleReset.bind(this);
-//     this.handleKeyValueChange - this.handleKeyValueChange.bind(this);
-//   }
-  
-//  handleChange(event) {
-//    console.log('target', event.target);
-//      this.setState({ [event.target.name]: event.target.value })
-// }
-
-// addKeyVal = (event) => {
-//     console.log(this.state.params);
-//     console.log("WE CLICKED THE ADD BUTTON") 
-//     console.log(this.state);  
-//     this.setState((prevState) => ({
-//       params: [...prevState.params, {key:"", value:""}],
-//     }));
-// }
-
-// handleSubmit() {
-//     console.log('BUTTON CLICKED')
-//     const SERVER_URL = "https://5000-f9e7f7e4-c57e-43fb-8340-f9648942ba09.ws-us02.gitpod.io/"
-//     console.log(SERVER_URL + '/api')
-//     const arrayToObject = (array) =>
-//         array.reduce((obj, item) => {
-//             if(item.key) {
-//                 obj[item.key] = item.value
-//             }
-//             return obj
-//     }, {})
- 
-//    const keyvalObject = arrayToObject(this.state.params)
-//    console.log(keyvalObject) 
-
-//         axios
-//             .post(SERVER_URL + 'api', {apiEndpoint: this.state.apiEndpoint, authToken: this.state.authToken, params: keyvalObject, Method:this.props.Method})
-//             .then((res) => {   
-//                 console.log(res.data)
-//                 this.setState({
-//                     response: res.data
-//                 })
-//             })   
-//     }  
-    
-//     handleReset(){
-//         this.setState({
-//             authToken: '',
-//             apiEndpoint: '',
-//             params: [{key:"", value:""}]
-//         });
-
-//     }
-
-//  handleKeyValueChange = (event, type, idx) => { 
-//      console.log("typed")   
-//     if(type === 'key') { 
-//         const newParams = [
-//             ...this.state.params.slice(0, idx),
-//             { key: event.target.value, value: this.state.params[idx].value }, 
-//             ...this.state.params.slice(idx + 1)
-//         ] 
-//         this.setState((prevState) => ({
-//             params: newParams
-//         }));
-//     }  
-//     if(type === 'value') {
-//         const newParams = [
-//             ...this.state.params.slice(0, idx),
-//             { key: this.state.params[idx].key, value: event.target.value }, 
-//             ...this.state.params.slice(idx + 1)
-//         ] 
-//         this.setState((prevState) => ({
-//             params: newParams
-//         }));
-//     }   
-// }
-
-// render(){
-//     let{authToken, apiEndpoint, params} = this.state
-//     return (
-//        <div className="inputForms">
-//           <center>
-//                 <label>API Endpoint</label><br />
-//                     <input className ="endpointBox" value={this.state.apiEndpoint}
-//                     type="text" name='apiEndpoint'onChange={this.handleChange} 
-//                     /> <br /> <br />
-//             <label>Authorization Token</label><br />
-//                 <input className ="authTokenBox" value={this.state.authToken}
-//                 type="text" name='authToken' onChange={this.handleChange} 
-//             /> <br /> <br />
-          
-//             <KVInputs handleChange={this.handleKeyValueChange} params={params}/>
-//              <button className = 'add' onClick={this.addKeyVal}>Add+</button>
-//              </center>
-//              <div className="resultsBox">  {/* results box code begins  */}
-//                     <center><h2>Results</h2></center> 
-//                     {/* <div> {paramsList}</div> */}
-//                     {/* <center>  */}
-//                         <ReactJson src={this.state.response} theme="bright:inverted"/>
-//                     {/* </center>  */}
-//             </div>   {/*  results box code ends  */} 
-//                 <br /> <button className = "submit" onClick={this.handleSubmit}>Submit</button>
-//                 <button className="resetbutton" onClick={this.handleReset}>Reset</button> 
-//         </div>
-//         )
-//     }   
-// } 
 
 export default Form;
--- src/index.jsx
+++ src/index.jsx
@@ -1,93 +1,70 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.scss';
-import axios from 'axios';
 import Form from './Form.jsx';
-import KVInputs from './KVInputs.jsx';
-
-if (module.hot) {
-    console.log('hot module');
-}
 
 const App = () => {
     const { useState } = React;
 
-    const [value2, setValue2] = useState('');
-    const [value, setValue] = useState('');
+    const [method, setMethod] = useState('');
+    const [endpoint, setEndpoint] = useState('');
     const [showForm, setShowForm] = useState(false);
     const [showIntro, setShowIntro] = useState(true);
 
-    const toggleButton = (param, param2) => {
-        setValue2(param2);
-        setValue(param);
-        setShowForm(true);
-        setShowIntro(false);
+    const toggleButton = (method, endpoint) => {
+      setMethod(method);
+      setEndpoint(endpoint);
+      setShowForm(true);
+      setShowIntro(false);
     }
 
     return (
-        <>
-        {/* <Form hello={'hello'} Method={value} /> */}
-          <div className="header"> {/*  dark blue full width banner code begins  */}
-              <h1>Bitly API Explorer</h1>
-          </div> {/* dark blue full width banner code ends */}
-          <div className="main">
-              <div className="sidebar"> {/* baby blue verticle banner code begins  */}
-              
-                  <div className="sidenav">
-                  <a href="#" onClick={() => toggleButton("GET",'/bitlinks/{Your_bitlink}')}><span className="Get">Retrieve</span> a Bitlink</a>
-                  <a href="#" onClick={() => toggleButton("GET",'/bitlinks/{Your_bitlink}/clicks')}><span className="Get">GET</span> clicks for a Bitlink</a>
-                  <a href="#" onClick={() => toggleButton("GET",'/groups/{Your_group_guid}/bitlinks')}><span className="Get">Retrieve</span> Bitlinks by Group</a>
-                  <a href="#" onClick={() => toggleButton("POST",'/expand')}><span className="Post">Expand</span> a Bitlink</a>
-                  <a href="#" onClick={() => toggleButton("POST",'/bitlinks')}><span className="Post">Create</span> a Bitlink</a>
-                  <a href="#" onClick={() => toggleButton("POST",'/shorten')}><span className="Post">Shorten</span> a Link</a>
-                  <a href="#" onClick={() => toggleButton("PATCH",'/bitlinks/{bitlink}')}><span className="Patch">Update</span> a Bitlink</a>
-                  <a href="#" onClick={() => toggleButton("PATCH",'/user')}><span className="Patch">Update</span> a User</a>
-                  <a href="#" onClick={() => toggleButton("PATCH",'/groups/{group_guid}')}><span className="Patch">Update</span> a Group</a>
-                  <a href="#" onClick={() => toggleButton("DELETE",'/groups/{group_guid}')}><span className="Delete">Delete</span> a Group</a>
-                  </div>
-                  
-              </div> {/* baby blue verticle banner code ends  */}
-
-              <div className="center">
-                  <div className="centerButtons"> {/* code for get post patch delete buttons begins */}
-                      <button className='methodButton getbutton'  onClick={() => toggleButton("GET")}>Get</button>
-                      <button className='methodButton postbutton' onClick={() => toggleButton("POST")}>Post</button>
-                      <button className='methodButton patchbutton' onClick={() => toggleButton("PATCH")}>Patch</button>
-                      <button className='methodButton deletebutton' onClick={() => toggleButton("DELETE")}>Delete</button>
-                  </div> {/* code for get post patch delete buttons ends  */}
-              
-                  <div className="centerCard">  {/* CENTER CARD WITH INTRO code begins  */}
-
-                  {
-                      showIntro ?
-                      <div className="intro">
-                         <p> Introduction: Welcome to the Bitly API! If you'd like to use Bitly to shorten, brand, share, 
-                          or retrieve data from links programmatically, you've come to the right place. 
-                          If you're interested in integrating</p>
-                          <p>
-                          your app or software platform with Bitly, you'll need to register and authenticate your service with our API. To do 
-                          so please contact us at API_sales@bit.ly </p> 
-                         <p>
-                          We currently provide our documentation in the form of an OpenAPI 2.0 document. 
-                          We do not support any code-generation at this time but feel free to use the specification if you would like. </p> 
-                      </div> : ' '
-                  }
-                  </div> {/* CENTER CARD WITH INTRO code ends   */}
-
-                  <div> {/*takes response from bitly api and turns it into json string  */}
-                      {
-                          showForm ?
-                          <Form Method={value} End={value2}/> : ''
-                      }
-                  </div> 
-              </div>
+      <>
+        <div className="header"> {/*  dark blue full width banner code begins  */}
+            <h1>Bitly API Explorer</h1>
+        </div>
+        <div className="main">
+          <div className="sidebar">
+            <div className="sidenav">
+            <a href="#" onClick={() => toggleButton("GET",'/bitlinks/{Your_bitlink}')}><span className="Get">Retrieve</span> a Bitlink</a>
+            <a href="#" onClick={() => toggleButton("GET",'/bitlinks/{Your_bitlink}/clicks')}><span className="Get">GET</span> clicks for a Bitlink</a>
+            <a href="#" onClick={() => toggleButton("GET",'/groups/{Your_group_guid}/bitlinks')}><span className="Get">Retrieve</span> Bitlinks by Group</a>
+            <a href="#" onClick={() => toggleButton("POST",'/expand')}><span className="Post">Expand</span> a Bitlink</a>
+            <a href="#" onClick={() => toggleButton("POST",'/bitlinks')}><span className="Post">Create</span> a Bitlink</a>
+            <a href="#" onClick={() => toggleButton("POST",'/shorten')}><span className="Post">Shorten</span> a Link</a>
+            <a href="#" onClick={() => toggleButton("PATCH",'/bitlinks/{bitlink}')}><span className="Patch">Update</span> a Bitlink</a>
+            <a href="#" onClick={() => toggleButton("PATCH",'/user')}><span className="Patch">Update</span> a User</a>
+            <a href="#" onClick={() => toggleButton("PATCH",'/groups/{group_guid}')}><span className="Patch">Update</span> a Group</a>
+            <a href="#" onClick={() => toggleButton("DELETE",'/groups/{group_guid}')}><span className="Delete">Delete</span> a Group</a>
           </div>
-
-          {/* logo image. for some reason it only shows if i have the image tag in the body not in the header idk why */}
-              <img className= "logo" alt="bitly logo"src="https://cdn.iconscout.com/icon/free/png-256/bitly-1-432498.png"/>
-
-              {/* end of json string code */}
-          </> 
+        </div>
+        <div className="center">
+          <div className="centerButtons">
+              <button className='methodButton getbutton'  onClick={() => toggleButton("GET")}>Get</button>
+              <button className='methodButton postbutton' onClick={() => toggleButton("POST")}>Post</button>
+              <button className='methodButton patchbutton' onClick={() => toggleButton("PATCH")}>Patch</button>
+              <button className='methodButton deletebutton' onClick={() => toggleButton("DELETE")}>Delete</button>
+          </div>
+          <div className="centerCard">
+            {
+              showIntro && <div className="intro">
+                <p>
+                  Introduction: Welcome to the Bitly API! If you'd like to use Bitly to shorten, brand, share, or retrieve data from links programmatically, you've come to the right place. If you're interested in integrating
+                </p>
+                <p>
+                  your app or software platform with Bitly, you'll need to register and authenticate your service with our API. To do so please contact us at API_sales@bit.ly
+                </p> 
+                <p>
+                  We currently provide our documentation in the form of an OpenAPI 2.0 document. We do not support any code-generation at this time but feel free to use the specification if you would like.
+                </p> 
+              </div>
+            }
+          </div>
+          { showForm && <Form endpoint={endpoint} method={method} /> }
+          </div>
+        </div>
+        <img className= "logo" alt="bitly logo"src="https://cdn.iconscout.com/icon/free/png-256/bitly-1-432498.png"/>
+      </> 
     );
 }
 
