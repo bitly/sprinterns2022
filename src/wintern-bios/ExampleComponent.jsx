266f923cdfde2c8cf318739e8a1c699bc99037a2
Andrea-Cajamarca <andreakcajamarca@gmail.com>
2021-01-05 21:22:18 +0000

    testing jsx file

--- src/wintern-bios/ExampleComponent.jsx
+++ src/wintern-bios/ExampleComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AndreasComponent from './AndreasComponent.jsx';
 
 const ExampleComponent = ({bioToShow, color='red'}) => {
   const { useState } = React;
@@ -16,7 +17,7 @@ const ExampleComponent = ({bioToShow, color='red'}) => {
         {/* { bioToShow === 'Dana' && <DanasComponent /> } */}
         {/* { bioToShow === 'Tajra' && <TajrasComponent /> } */}
         {/* { bioToShow === 'Lara' && <LarasComponent /> } */}
-        {/* { bioToShow === 'Andrea' && <AndreasComponent /> } */}
+        { bioToShow === 'Andrea' && <AndreasComponent /> }
         {/* { bioToShow === 'Roma' && <RomasComponent /> } */}
     </ div>
   );
