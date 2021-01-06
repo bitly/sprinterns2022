c54ee8d53102ef13240f31fb36424e263923cc9f
rcooper3307 <rcooper3307@bths.edu>
2021-01-06 15:45:03 +0000

    Testing, I think

--- src/wintern-bios/ExampleComponent.jsx
+++ src/wintern-bios/ExampleComponent.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
 import RomasComponent from './RomasComponent.jsx';
-=======
 import LarasComponent from './LarasComponent.jsx';
 import AndreasComponent from './AndreasComponent.jsx';
 import TajrasComponent from './TajrasComponent.jsx';
->>>>>>> af4e9cd5072e1759a3131528d758e5899d5c7f8b
 
 const ExampleComponent = ({bioToShow, color='red'}) => {
   const { useState } = React;
@@ -20,19 +17,11 @@ const ExampleComponent = ({bioToShow, color='red'}) => {
         <p className={color}>Random Number: {randomNum}</p>
         <p>{bioToShow}</p>
 
-<<<<<<< HEAD
-        {/*{ bioToShow === 'Dana' && <DanasComponent /> } */}
-        {/*{ bioToShow === 'Tajra' && <TajrasComponent /> } */}
-        {/*{ bioToShow === 'Lara' && <LarasComponent /> } */}
-        {/*{ bioToShow === 'Andrea' && <AndreasComponent /> } */}
-        { bioToShow === 'Roma' && <RomasComponent /> } 
-=======
         {/* { bioToShow === 'Dana' && <DanasComponent /> } */}
         { bioToShow === 'Tajra' && <TajrasComponent /> } 
         { bioToShow === 'Lara' && <LarasComponent /> } 
         { bioToShow === 'Andrea' && <AndreasComponent /> }
-        {/* { bioToShow === 'Roma' && <RomasComponent /> } */}
->>>>>>> af4e9cd5072e1759a3131528d758e5899d5c7f8b
+        { bioToShow === 'Roma' && <RomasComponent /> }
     </ div>
   );
 }
