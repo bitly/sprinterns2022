602fe6c55811e203006a408c92fafa37028d6a78
rcooper3307 <rcooper3307@bths.edu>
2021-01-07 22:20:13 +0000

    barebones template

--- src/wintern-bios/RomasComponent.jsx
+++ src/wintern-bios/RomasComponent.jsx
@@ -13,12 +13,27 @@ const RomasComponent = ({bioToShow, color='red'}) => {
        
         <p>{bioToShow}</p>
         <p>Roma Cooper</p>
+        <img className="valorant" alt="test image"src="https://riot.com/2Lm40MF"/>
+        <hr/>
         <p className="testtag">About Me</p>
         <p>Hey I'm Roma I like to play a total of three video games and this is the one im playing right now this is just placeholder</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-        <hr />
-        <img className="valorant" alt="test image"src="https://riot.com/2Lm40MF"/>
+        <hr/>
+        <p>Things That I Like</p>
+        <p>bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh  </p>
+        <p>bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh  </p>
+        <p>bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh  </p>
+        <p>bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh  </p>
+        <hr/>
+        <p>What I Learned In Boating School Today Is</p>
+        <p>bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh  </p>
+        <p>bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh  </p>
+        <p>bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh  </p>
+        <p>bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh bluh  </p>
+        <hr/>
+
+        
     </ div>
   );
 }
