d5287999576551f1292221fea84cee5a249c3eca
danahf <danahf98@gmail.com>
2021-01-07 23:04:36 +0000

    newest changes added

--- src/wintern-bios/DanasComponent.jsx
+++ src/wintern-bios/DanasComponent.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 
 const DanasComponent = ({bioToShow, color='red'}) => {
-  const { useState } = React;
+    const { useState } = React;
 
-  const [showStateEx, setShowStateEx] = useState(false);
+    const [showStateEx, setShowStateEx] = useState(false);
 
-  const randomNum = Math.random();
-
-
-  return (
-    <div className='dana-bio'>
-        <h1 className='title'> Dana Fishman </h1>
-        <hr/>
-        <p>Hello there!</p> 
-        <img className= "dana" alt="Dana Fishman"src="https://bit.ly/3onMGFx"/>
-        <img className= "caticorn" alt="very cute kitty"src="https://www.flaticon.com/svg/static/icons/svg/3468/3468377.svg"/>
-        <img className= "pride" alt="lgbt"src="https://www.flaticon.com/svg/static/icons/svg/2210/2210724.svg"/>
-    </ div> 
-  );
+    return (
+        <div className='dana-bio'>
+            <section class="showcase"></section>
+            <hr/>
+            <img className= "dana" alt="Dana Fishman"src="https://bit.ly/3onMGFx"/>
+            <img className= "caticorn" alt="very cute kitty"src="https://www.flaticon.com/svg/static/icons/svg/3468/3468377.svg"/>
+            <img className= "pride" alt="lgbt"src="https://www.flaticon.com/svg/static/icons/svg/2210/2210724.svg"/>
+            <div class="text">
+                <h2>Dana Fishman</h2> 
+                <h3>World's Coolest Person</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.</p>
+            </div>
+        </div> 
+    );
 }
 
 export default DanasComponent;
