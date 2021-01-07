7173d0d6018029504fe3be7649cb40dc0e231ec5
rcooper3307 <rcooper3307@bths.edu>
2021-01-07 19:26:55 +0000

    bluh bluh

--- src/wintern-bios/RomasComponent.jsx
+++ src/wintern-bios/RomasComponent.jsx
@@ -17,6 +17,7 @@ const RomasComponent = ({bioToShow, color='red'}) => {
         <p>Hey I'm Roma I like to play a total of three video games and this is the one im playing right now this is just placeholder</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        <hr />
         <img className="valorant" alt="test image"src="https://riot.com/2Lm40MF"/>
     </ div>
   );
