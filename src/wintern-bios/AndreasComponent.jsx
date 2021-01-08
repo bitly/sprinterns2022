c0047ed2ceecc2f2a1b08b5b17911bcf8287ff1a
Andrea-Cajamarca <andreakcajamarca@gmail.com>
2021-01-08 22:00:03 +0000

    took out extra divs, working on bg img style

--- src/wintern-bios/AndreasComponent.jsx
+++ src/wintern-bios/AndreasComponent.jsx
@@ -18,20 +18,13 @@ const AndreasComponent = ({bioToShow, color='red'}) => {
                     <img className = "andreaImg" src={img} />
                 </div>
             </div>
-            <div className="flex-container">
-            <div className = "paraLeft">
+            <div className = "paragraphs">
             <p>About Me: I am 21-years-old. I am a Junior in CUNY Hunter College. My major is English, concentrated on Creative Writing; minor in Computer Science. Fun Fact: I have plenty! I am the third oldest of 7 kids. I have a 5-year-old cat named Khloe! I know two other languages (Spanish, and Japanese).</p>
-             </div>
-            
-            <div className= "paraCenter">
-                <p>Things I Like: I like to write! I am currently writing an epic that I've planned out for many years. It is currently being revised, and will continue to be revised until I have written every idea out (it may take a while). I also like to play softcore and relaxing games such as Animal Crossing, Harvest Moon, and Minecraft!</p>
-               </div>
-            
-            <div className= "paraRight">
-                <p>Things I Like: I like to write! I am currently writing an epic that I've planned out for many years. It is currently being revised, and will continue to be revised until I have written every idea out (it may take a while). I also like to play softcore and relaxing games such as Animal Crossing, Harvest Moon, and Minecraft!</p>
-               </div>
 
-               </div>
+            <p>Things I Like: I like to write! I am currently writing an epic that I've planned out for many years. It is currently being revised, and will continue to be revised until I have written every idea out (it may take a while). I also like to play softcore and relaxing games such as Animal Crossing, Harvest Moon, and Minecraft!</p>
+            
+            <p>Things I Like: I like to write! I am currently writing an epic that I've planned out for many years. It is currently being revised, and will continue to be revised until I have written every idea out (it may take a while). I also like to play softcore and relaxing games such as Animal Crossing, Harvest Moon, and Minecraft!</p>
+            </div>
         </ div>
 </div>
   );
