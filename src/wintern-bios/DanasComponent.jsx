8d54620b70efe480202c2bcec4565f8d1f365302
danahf <danahf98@gmail.com>
2021-01-08 19:41:38 +0000

    remove name and #

--- src/wintern-bios/DanasComponent.jsx
+++ src/wintern-bios/DanasComponent.jsx
@@ -5,21 +5,24 @@ const DanasComponent = ({bioToShow, color='red'}) => {
 
     const [showStateEx, setShowStateEx] = useState(false);
 
+    const randomNum = Math.random();
+
     return (
         <div className='dana-bio'>
-            <section class="showcase"></section>
-            <hr/>
-            <img className= "dana" alt="Dana Fishman"src="https://bit.ly/3onMGFx"/>
-            <img className= "caticorn" alt="very cute kitty"src="https://www.flaticon.com/svg/static/icons/svg/3468/3468377.svg"/>
-            <img className= "pride" alt="lgbt"src="https://www.flaticon.com/svg/static/icons/svg/2210/2210724.svg"/>
-            <div class="text">
-                <h2>Dana Fishman</h2> 
-                <h3>World's Coolest Person</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                consequat.</p>
-            </div>
+            <section class="showcase">
+                <img className= "dana" alt="Dana Fishman"src="https://bit.ly/3onMGFx"/>
+                <img className= "caticorn" alt="very cute kitty"src="https://www.flaticon.com/svg/static/icons/svg/3468/3468377.svg"/>
+                <img className= "pride" alt="lgbt"src="https://www.flaticon.com/svg/static/icons/svg/2210/2210724.svg"/>
+                <div class="text">
+                    <h2>Dana Fishman</h2> 
+                    <h3>World's Coolest Person</h3>
+                    <hr/>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.</p>
+                </div>
+            </section>
         </div> 
     );
 }
