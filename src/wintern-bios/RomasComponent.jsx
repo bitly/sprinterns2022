b981241d7aa2b27ddbd430bfb4d32c2e83d638b7
rcooper3307 <rcooper3307@bths.edu>
2021-01-08 21:47:27 +0000

    just gonna do this before my wifi immediately dies

--- src/wintern-bios/RomasComponent.jsx
+++ src/wintern-bios/RomasComponent.jsx
@@ -12,8 +12,8 @@ const RomasComponent = ({bioToShow, color='red'}) => {
     <div className='romabio'>
        
         <p>{bioToShow}</p>
+        <img className="selfportrait" alt="test image"src="https://i.imgur.com/Eb9wkez.png"/>
         <h1>Roma Cooper</h1>
-        <img className="valorant" alt="test image"src="https://i.imgur.com/Eb9wkez.png"/>
         <hr/>
         <p className="testtag">About Me</p>
         <p>Hey there! My name is Roma! </p>
@@ -21,13 +21,13 @@ const RomasComponent = ({bioToShow, color='red'}) => {
         <p>My pronouns are she/they.</p>
         <p>I'm a sophomore at Brooklyn College majoring in Computer Science.</p>
         <hr/>
-        <p>Things That I Like</p>
+        <p className="testtag">Things That I Like</p>
         <p>Games! I love playing Valorant, Overwatch, and Minecraft. Even though I only play a total of three games, I never get exhausted of them, so it's not a problem. </p>
         <p>I also love drawing and have recently been working a lot on my digital art!</p>
         <p>I also love all my friends!!!</p>
         
         <hr/>
-        <p>What I Want To Be When I Grow Up?</p>
+        <p className="testtag">What I Want To Be When I Grow Up</p>
         <p>I'm still not sure! Is that an okay thing to admit at age 19? That I still haven't completely mapped out exactly what it is</p>
         <p>that I want to accomplish in life by this tender age? All I know is that I'm really curious about Computer Science things and Graphic Design! </p>
     
