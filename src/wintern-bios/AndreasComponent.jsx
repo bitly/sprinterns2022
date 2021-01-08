622382150fde58ea4c93e4923f231ac61da7e4c4
Andrea-Cajamarca <andreakcajamarca@gmail.com>
2021-01-08 20:37:50 +0000

    styles with flexbox

--- src/wintern-bios/AndreasComponent.jsx
+++ src/wintern-bios/AndreasComponent.jsx
@@ -12,9 +12,13 @@ const AndreasComponent = ({bioToShow, color='red'}) => {
   return (
     <div className = "andreaBGI">  
         <div className='andreasBio'>
-            <h1>Bio: Andrea K Cajamarca</h1>
-
-            <img className = "andreaImg" src={img} />
+            <div class="container">
+  <             div class="jumbotron">
+                    <h1>Bio: Andrea K Cajamarca</h1>
+                    <img className = "andreaImg" src={img} />
+                </div>
+            </div>
+            <div className="flex-container">
             <div className = "paraLeft">
             <p>About Me: I am 21-years-old. I am a Junior in CUNY Hunter College. My major is English, concentrated on Creative Writing; minor in Computer Science. Fun Fact: I have plenty! I am the third oldest of 7 kids. I have a 5-year-old cat named Khloe! I know two other languages (Spanish, and Japanese).</p>
              </div>
@@ -25,6 +29,8 @@ const AndreasComponent = ({bioToShow, color='red'}) => {
             
             <div className= "paraRight">
                 <p>Things I Like: I like to write! I am currently writing an epic that I've planned out for many years. It is currently being revised, and will continue to be revised until I have written every idea out (it may take a while). I also like to play softcore and relaxing games such as Animal Crossing, Harvest Moon, and Minecraft!</p>
+               </div>
+
                </div>
         </ div>
 </div>
