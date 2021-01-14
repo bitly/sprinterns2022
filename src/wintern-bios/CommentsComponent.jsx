86de57c4c9c9efd1b3a61a1774d1497658fc7eb5
Tajra Sinanagic <tajra.sinanagic24@students.kbcc.cuny.edu>
2021-01-14 20:53:36 +0000

    Adding new file Comments

---
+++ src/wintern-bios/CommentsComponent.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CommentsComponent = ({bioToShow, color='red'}) => {
+  const { useState } = React;
+
+  const [showStateEx, setShowStateEx] = useState(false);
+  const randomNum = Math.random();
+
+
+  return (
+    <div className='commentsset'>
+    
+    
+        </div>
+
+  );
+        }
+
+export default CommentsComponent;
