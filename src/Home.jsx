60ec5665176200bf6ae6d78903db71e14d9c6b37
Lara Choudhury <larachoudhury@Laras-MBP.fios-router.home>
2021-01-20 14:37:01 -0500

    small changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -99,6 +99,13 @@ const Home = ({bioToShow}) => {
               </div>
               
             }
+            <div className="card">
+              <img className="card-img-top" src="Lara_Image.jpg" alt="Card image cap" ></img>
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere </a>
+              </div>
           </div>
           <div className="footer"> 
            <div className="footertext">
