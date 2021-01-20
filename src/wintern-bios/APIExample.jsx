816bbbb85d28a0a31b5a8045aaccb47ce5930b57
Andrea-Cajamarca <andreakcajamarca@gmail.com>
2021-01-20 18:43:56 +0000

    css center poster img

--- src/wintern-bios/APIExample.jsx
+++ src/wintern-bios/APIExample.jsx
@@ -8,6 +8,7 @@ const APIExample = () => {
 
   const [data, setData] = useState({});
 
+
 const getMovie = () => {
     fetch(
       "https://api.themoviedb.org/3/search/movie?query=" + title + "&api_key=a11be34c1389ded0caa97d40a55dfe00")
@@ -22,18 +23,19 @@ const getMovie = () => {
 
 
   return (
-       <div>
+       <div className= "api-form">
           <form>
-            Title
-          <input type="text" name="title" onChange={(event) => setTitle(event.target.value) }/>
+            <h1 className="categoryTitle">Title</h1>
+          <input type="text" onChange={(event) => setTitle(event.target.value) }/>
           <input type="submit" id="button" onClick={() => getMovie() }/>
             {title}
           </form>
-          {/* {data && (<img src='https://image.tmdb.org/t/p/w500/' + {data.poster_path}+ >)}  */}
-          {data && (<p>{data.title}</p>)}
-          {data && (<p>{data.original_language}</p>) }
-          {data && (<p>{data.release_date}</p>) }
-          {data && (<p>{data.vote_average}</p>) }  
+          {data && (<img className = "poster" src = {'https://image.tmdb.org/t/p/w500/' + data.poster_path}></img>)}
+          {data && (<p>Title: {data.title}</p>)}
+          {data && (<p>Language: {data.original_language}</p>) }
+          {data && (<p>Overview: {data.overview}</p>) }
+          {data && (<p>Release Date: {data.release_date}</p>) }
+          {data && (<p> Vote Average: {data.vote_average}</p>) }  
           </div>
   );
 }
