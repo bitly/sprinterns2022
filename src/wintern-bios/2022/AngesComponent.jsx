cf9aabfb4c6f7a476fcf5d3a7e52030e1019276b
Ange Louis <alouis@anges-mbp-16.myfiosgateway.com>
2022-01-06 17:02:23 -0500

    Ange's Component file added and rendered in TeamLandingPage.jsx

---
+++ src/wintern-bios/2022/AngesComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AngesComponent = () => {
+
+    return (
+
+        <div>
+        OMG. IT'S Ange BLANK WEBSITE!
+        </ div>
+    );
+
+}
+
+export default AngesComponent;
--- src/wintern-bios/2022/TeamLandingPage2022.jsx
+++ src/wintern-bios/2022/TeamLandingPage2022.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import AngesComponent from './AngesComponent.jsx'
 
 const TeamLandingPage2022 = () => {
   return (
     <div>
         OMG. IT'S A BLANK WEBSITE!
+       { <AngesComponent />}
     </ div>
+   
   );
 }
 
