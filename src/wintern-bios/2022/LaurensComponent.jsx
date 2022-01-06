aca9c082dc3fc7747c077a97def3386ca864ffc9
Diana Bishop <diana@bit.ly>
2022-01-06 16:25:10 -0500

    example start

---
+++ src/wintern-bios/2022/LaurensComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LaurensComponent = () => {
+  return (
+    <div>HELLO IT'S LAUREN</div>
+  );
+}
+
+export default LaurensComponent;
--- src/wintern-bios/2022/TeamLandingPage2022.jsx
+++ src/wintern-bios/2022/TeamLandingPage2022.jsx
@@ -4,6 +4,7 @@ const TeamLandingPage2022 = () => {
   return (
     <div>
         OMG. IT'S A BLANK WEBSITE!
+        { <LaurensComponent /> }
     </ div>
   );
 }
