f338edfa15acf1c9adcac7ed9e86bc4a2eedac18
Lauren Avilla <laurenavilla@Laurens-MacBook-Air.local>
2022-01-07 13:01:25 -0500

    added Lauren bio for the first time

---
+++ src/wintern-bios/2022/LaurenComponent.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const LaurenComponent = () => {
+    return (
+        <div className = "laurenbio">
+            <h1 className = "fullname">Lauren Avilla</h1>
+            <h1 className = "grade">Freshman @ Hunter College</h1>
+            <h1 className = "major">Major: Computer Science</h1>
+            <h1 className = "location"> Long Island, NY</h1>
+            <hr/>
+            <h2 className = "aboutme">About Me</h2>
+            <h2 className = "whyCS">Why CS?</h2>
+            <h2 className = "bitly">Why I love being a Sprintern @ Bitly</h2>
+            <hr/>
+            <h2 className = "career">Where I see myself in 5 years</h2>
+            <h2 className = "hobbies">Hobbies</h2>
+            <h2 className = "media">Favorite Media</h2>
+        </div>
+    );
+}
+
+export default LaurenComponent;
