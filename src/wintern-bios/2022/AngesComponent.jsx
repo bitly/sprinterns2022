ad596e771f1d78764601be6f16af9ad85f8cf6fe
Ange Louis <alouis@anges-mbp-16.myfiosgateway.com>
2022-01-06 22:48:49 -0500

    Added an image to the page

--- src/wintern-bios/2022/AngesComponent.jsx
+++ src/wintern-bios/2022/AngesComponent.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import img from '../../assets/bio-photos/79405.jpg';
 
 const AngesComponent = () => {
 
     return (
 
+        
+
         <div>
+            <img src = {img}/>
         OMG. IT'S Ange BLANK WEBSITE!
+        
         </ div>
     );
 
