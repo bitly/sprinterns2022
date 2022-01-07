4c96daefc1c753c51f6f45528deffbeb2e0a5b7e
Skyler Basco <sbasco@Skylers-MBP-16.home>
2022-01-07 09:17:55 -0500

    google fonts api

--- src/wintern-bios/2022/SkylerComponent.jsx
+++ src/wintern-bios/2022/SkylerComponent.jsx
@@ -9,10 +9,13 @@ const SkylerComponent = () => {
         <p className='about-content'> My name is Skyler! I’m a developer and designer with an affinity for creative mediums in tech, including augmented/virtual reality, 3D software, and visual effects.
         At Queens College, I’m currently studying Computer Science. I aspire to transform our reality’s “impossibilities” into immersive VR games and AR experiences that uplift people and inspire them to create. </p>
         <h1 className='code'> Code 👩🏻‍💻</h1>
+        <p className='code-content'>Check out some of the projects I've developed below!</p>
 
         <h1 className='create'> Create 🎨</h1>
+        <p className='create-content'>I love playing around with different artistic mediums - ranging from colored pencil realism to digital art. </p>
 
         <h1 className='play'> Play 👾</h1>
+        <p className='play-content'>In my free time, you can find me...</p>
 
         <h1 className='contact'> Contact 📩</h1>
         <p className='contact-content'>Want to connect, collaborate, or build something innovative together? I’m happy to chat about anything from CS to design to future projects. Feel free to shoot me a DM on LinkedIn or send a message through the form below! ⬇️</p>
