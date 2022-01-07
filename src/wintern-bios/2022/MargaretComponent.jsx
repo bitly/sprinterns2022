7744a8ae18105a254e6be4412988fe86f96047eb
Margaret Diaz <mdiaz@MargaretsMBP16.fios-router.home>
2022-01-07 13:01:38 -0500

    updated margaret component and started style

--- src/wintern-bios/2022/MargaretComponent.jsx
+++ src/wintern-bios/2022/MargaretComponent.jsx
@@ -4,11 +4,79 @@ var randomColor = Math.floor(Math.random()*16777215).toString(16);
 const MargaretComponent = () => {
  return (
     <div>
-      <p> Margaret Diaz Bio </p>
-      
+      <h1> MARGARET DIAZ </h1>
+      <hr/>
+      <h2>ABOUT ME</h2>
+      <p>I am a 23 sophomore at Hunter College majoring in Computer Science. I recently moved to the city after transferring from Bryn Mawr College in PA. My first expereince with coding was the art-focused language of processing and ever since then I've been hooked! I love the trial and error process of coding and debugging, it's like solving a jigsaw puzzle. My dream job is to become a software engineer and I am thankful to every opportunity that brings me closer to this path. I am incredibly grateful to my family. 
+        My parents had to sacrafice so much to provide for me and my siblings and as a first generation college student a goal of mine is to graduate, complete my masters, and buy my family a big house! I also would like to retire on a farm, foster pets and have a pet pig (they love to cuddle!)</p>
+      <hr/>
+      <h2>FAVORITES:</h2>
+      <ul>
+        <li>Music! Here are some of my favorite songs (it was so hard to only pick 10)</li>
+        <ul>
+          <li>Taylor Swift</li>
+          <li>Esteman</li>
+          <li>Ruzzi</li>
+          <li>Paramore (never outgrown my angsty phase XD)</li>
+          <li>Doja Cat</li>
+          <li>Labrynth</li>
+          <li>Supply and Demand</li>
+          <li>TWICE</li>
+          <li>Bad Bunny</li>
+          <li>Britney Spears</li>
+        </ul>
+        <li>My pets (add pictures)</li>
+        <li>Fandoms</li>
+        <ul>
+          <li>Marvel</li>
+          <li>DC</li>
+          <li>AOT</li>
+          <li>What We do in the Shadows</li>
+          <li>GOT and ASOIF (but let's just forget the last season)</li>
+          <li>MHA</li>
+          <li>Star Wars</li>
+          <li>Too many to list ...</li>
+        </ul>
+        <li>Jigsaw puzzles</li>
+        <li>Digital Art (it's not good but its fun!)</li>
+        <li>Writing (it's also not good but its fun!)</li>
+        <li>True Crime Podcasts</li>
+        <li>Reality TV Shows/Movies that make you loose brain cells (the best kind)</li>
+      </ul>
+      <hr/>
+      <h2>FUN FACTS</h2>
+      <ul>
+        <li>I have eight siblings (but sadly never got a TLC show for it)</li>
+        <li>I have completed over 50 puzzles during quarentine</li>
+        <li>It has been a long journey to get to Hunter and in that time I have worked a wiiiide range of jobs</li>
+        <ul>
+          <li>Robotics Instructor (We got to play with glorified legos! 10/10)</li>
+          <li>At the Circus! (Free food but no free show 6/10)</li>
+          <li>Data Entry/Lab Assistant (Nice paycheck and finally got comfortable with excel 8/10)</li>
+          <li>Pre-School Teacher Assistant (Love the kids, hate the diapers 9/10)</li>
+          <li>Vet tech (Got to meet so many pets but but also had to bag dead cats 3/10))</li>
+          <li>Bookkeeping assistant (Quickbooks is the bane of my exisistance 5/10)</li>
+          <li>Cashier (Loved my coworkers but we didn't get store discounts on groceries 4/10)</li>
+          <li>Receptionist (Loved interacting with people just not a fan of phonecalls 6/10)</li>
+          <li>Nanny (The kids were so sweet! 10/10)</li>
+          <li>Dog Walker and sitter (Pets are always fun 10/10) </li>
+          <li>Library shelver (Got me back into reading 8/10)</li>
+        </ul>
+      </ul>
     </div>
  );
 }
+
+// make circus animation, thing move in background
+//picture of me
+// move name back and forth
+//art woork
+//picture of pets / click they give description
+//jigsaw puzzle
+//family facts
+//fun fonts
+//fandoms im in
+//maybe api
 
 //function FunColors() {
 // for (i = 0; i < 10; i++) {
@@ -16,10 +84,11 @@ const MargaretComponent = () => {
  // }
 //}
 
-setInterval(
-  function () {
-    var randomColor = Math.floor(Math.random()*16777215).toString(16);
-    document.body.style.backgroundColor = "#"+randomColor;
-  },1000);
+
+//setInterval(
+  //function () {
+    //var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    //document.body.style.backgroundColor = "#"+randomColor;
+  //},1000);
 
 export default MargaretComponent;
