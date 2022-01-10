448141101dcffc70cee9f596ec648fe35973536c
Ange Louis <alouis@anges-mbp-16.myfiosgateway.com>
2022-01-10 13:52:11 -0500

    Finished the contact info box, Added the about me box and typewriter effect

--- src/wintern-bios/2022/AngesComponent.jsx
+++ src/wintern-bios/2022/AngesComponent.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import img from '../../assets/bio-photos/79405.jpg';
 
+const App = () => {
+    const content = 'If life is so blue, then select another colour from the rainbow';
+
+    const ele = '<span>' + content.split('').join('</span><span>') + '</span>';
+
+}
+
 const AngesComponent = () => {
 
     return (
@@ -14,8 +21,20 @@ const AngesComponent = () => {
             </div>
             <img src = {img}/>
             <div className='contact'>
-                <h3>CONTACT INFORMATION &#x1F4DE;</h3>
+               <div><h3>CONTACT INFORMATION &#x1F4DE;</h3></div>
+               <div><h4>LinkedIn: linkedin.com/in/ange-louis</h4></div> 
+               <div><h4>Github: github.com/angelouis</h4></div> 
                 
+            </div>
+            <div className='aboutme'>
+             <p class="type">Here is some information about me :) </p>
+
+             <p class="type">Tim Hortons queues too keener to the donair, and its 
+             garburator -- igloo Canuck melted, eh? Maclean's magazine ranked pop first of tuque beer Canada's puck pissed 
+             Tim Hortons hoser.</p>
+
+             <p class="type">My name is Ange Louis and I am </p>
+
             </div>
             
         
