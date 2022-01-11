fde619ee425d5d32dd3b4bf72ada012e018fcbed
Margaret Diaz <mdiaz@MargaretsMBP16.fios-router.home>
2022-01-11 10:07:46 -0500

    “your-commit-message”

--- src/wintern-bios/2022/MargaretComponent.jsx
+++ src/wintern-bios/2022/MargaretComponent.jsx
@@ -1,36 +1,60 @@
 import React from 'react';
-//import Animate from 'animate.css-react'
-//import 'animate.css/animate.css' 
-// var randomColor = Math.floor(Math.random()*16777215).toString(16);
-
+//var randomColor = Math.floor(Math.random()*16777215).toString(16);
+import img from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/bio-photos/PROFILE.JPEG';
+import bella from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/bella.jpg';
+import broady from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/broady.jpg';
+import jason from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/jason.png';
+import sharon from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/sharon.png';
+import stevie from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/stevie.png';
+import winnie from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/winnie.png';
+import olddoggies from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/olddoggies.png';
+import infinity from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/infinity.png';
+import quinn from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/quinn.png';
 
 const MargaretComponent = () => {
  return (
-    <div>
-      <div className='animateName'> MARGARET DIAZ </div>
-      <hr/>
-      <h2>ABOUT ME</h2>
-      <p>I am a 23 sophomore at Hunter College majoring in Computer Science. I recently moved to the city after transferring from Bryn Mawr College in PA. My first expereince with coding was the art-focused language of processing and ever since then I've been hooked! I love the trial and error process of coding and debugging, it's like solving a jigsaw puzzle. My dream job is to become a software engineer and I am thankful to every opportunity that brings me closer to this path. I am incredibly grateful to my family. 
-        My parents had to sacrafice so much to provide for me and my siblings and as a first generation college student a goal of mine is to graduate, complete my masters, and buy my family a big house! I also would like to retire on a farm, foster pets and have a pet pig (they love to cuddle!)</p>
-      <hr/>
-      <h2>FAVORITES:</h2>
-      <ul>
-        <li>Music! Here are some of my favorite songs (it was so hard to only pick 10)</li>
-        <ul>
-          <li>Taylor Swift</li>
-          <li>Esteman</li>
-          <li>Ruzzi</li>
-          <li>Paramore (never outgrown my angsty phase XD)</li>
-          <li>Doja Cat</li>
-          <li>Labrynth</li>
-          <li>Supply and Demand</li>
-          <li>TWICE</li>
-          <li>Bad Bunny</li>
-          <li>Britney Spears</li>
-        </ul>
-        <li>My pets (add pictures)</li>
-        <li>Fandoms</li>
-        <ul>
+    <div className='backgroundMe'>  
+      <h1 className='borderName'>  MARGARET DIAZ </h1>
+      <img className='profilePic' src={img}/>
+      <h2 className='profileName'>(⊙⊙) Hover over the images for a surprise (⊙⊙)</h2>
+      <p className= 'aboutME'>I am a sophomore at Hunter College majoring in Computer Science. I recently moved to the city after transferring from Bryn Mawr College in PA. My first expereince with coding was the creative code sketchbook Processing. I love the trial and error process of coding and debugging, it's like solving a jigsaw puzzle! My dream job is to become a software engineer and I am thankful to every opportunity that brings me closer to this path. I am incredibly grateful to my family. 
+        My parents had to sacrafice so much to provide for me and my siblings and as a first generation college student a life goal of mine is to graduate, complete my masters, and buy my family a big house! I also would like to retire on a farm, foster pets and have a pet pig (they love to cuddle!)</p>
+      <img className='bellaImg' src={bella}/>
+      <h2 className='bellaName'> 💜 Bella 🧡</h2>
+      <img className='broadyImg' src={broady}/>
+      <h2 className='broadyName'> 💚 Broady 🤎 </h2>
+      <img className='jasonImg' src={jason}/>
+      <h2 className='jasonName'> 💛 Jason 🤍 </h2>
+      <img className='winnieImg' src={winnie}/>
+      <h2 className='winnieName'> 💙 Winnie 💜 </h2>
+      <img className='sharonImg' src={sharon}/>
+      <h2 className='sharonName'> 🧡 Sharon 💚 </h2>
+      <img className='stevieImg' src={stevie}/>
+      <h2 className='stevieName'> 🤎 Stevie 💛 </h2>
+      <img className='olddoggiesImg' src={olddoggies}/>
+      <h2 className='olddoggiesName'> 🤍 Steve + Sharon 💜</h2>
+      <a className='favoriteMusic'>♬ Favorite Songs ♬</a>
+      <a className='clicktoplay'>♬Click to listen♬</a>
+      <a href="https://www.youtube.com/watch?v=2d1wKn-oJnA" className='taylorButton'>Taylor Swift</a>
+      <a href="https://www.youtube.com/watch?v=sVdxA0rFN5c" className='estemanButton'>Esteman</a>
+      <a href="https://www.youtube.com/watch?v=jix-u8h4KEU" className='hayleyButton'>Hayley Kiyoko</a>
+      <a href="https://www.youtube.com/watch?v=QIQvUmiyVTY" className='ruzziButton'>Ruzzi</a>
+      <a href="https://www.youtube.com/watch?v=RvnkAtWcKYg" className='paramoreButton'>Paramore</a>
+      <a href="https://www.youtube.com/watch?v=XBCDvINm0Vo"className='selenaButton'>Selena</a>
+      <a href="https://www.youtube.com/watch?v=56WBK4ZK_cw"className='khalidButton'>Khalid</a>
+      <a href="https://www.youtube.com/watch?v=owLnDbAIOWE"className='mirandaButton'>Miranda Del Sol</a>
+      <a href="https://www.youtube.com/watch?v=kOHB85vDuow"className='twiceButton'>Twice</a>
+      <a href="https://www.youtube.com/watch?v=TmKh7lAwnBI"className='badBunny'>Bad Bunny</a>
+      <a href="https://www.youtube.com/watch?v=clwLKJ294u4"className='britneyButton'>Britney Spears</a>
+      <a href="https://www.youtube.com/watch?v=n5LWsc-qCdA"className='meganButton'>Megan Thee Stallion</a>
+      <a className='favoriteFacts'> Extra </a>
+          <h2 className='center'>FAVORITES:</h2>
+          <a className='myFavs'>
+          <li>Jigsaw puzzles</li>
+          <li>Digital Art (it's not good but its fun!)</li>
+          <li>Writing (it's also not good but its fun!)</li>
+          <li>True Crime Podcasts</li>
+          <li>Reality TV Shows/Movies that make you loose brain cells (the best kind)</li>
           <li>Marvel</li>
           <li>DC</li>
           <li>AOT</li>
@@ -38,60 +62,27 @@ const MargaretComponent = () => {
           <li>GOT and ASOIF (but let's just forget the last season)</li>
           <li>MHA</li>
           <li>Star Wars</li>
-          <li>Too many to list ...</li>
-        </ul>
-        <li>Jigsaw puzzles</li>
-        <li>Digital Art (it's not good but its fun!)</li>
-        <li>Writing (it's also not good but its fun!)</li>
-        <li>True Crime Podcasts</li>
-        <li>Reality TV Shows/Movies that make you loose brain cells (the best kind)</li>
-      </ul>
-      <hr/>
-      <h2>FUN FACTS</h2>
+          <li>Much More... it never ends</li>
+      </a>
+      <img className='tooMuch' src={infinity}/>
+      <h2 className='centerFacts'>Facts</h2>
+      <a className ='Facts'>
       <ul>
         <li>I have eight siblings (but sadly never got a TLC show for it)</li>
-        <li>I have completed over 50 puzzles during quarentine</li>
-        <li>It has been a long journey to get to Hunter and in that time I have worked a wiiiide range of jobs</li>
-        <ul>
-          <li>Robotics Instructor (We got to play with glorified legos! 10/10)</li>
-          <li>At the Circus! (Free food but no free show 6/10)</li>
-          <li>Data Entry/Lab Assistant (Nice paycheck and finally got comfortable with excel 8/10)</li>
-          <li>Pre-School Teacher Assistant (Love the kids, hate the diapers 9/10)</li>
-          <li>Vet tech (Got to meet so many pets but but also had to bag dead cats 3/10))</li>
-          <li>Bookkeeping assistant (Quickbooks is the bane of my exisistance 5/10)</li>
-          <li>Cashier (Loved my coworkers but we didn't get store discounts on groceries 4/10)</li>
-          <li>Receptionist (Loved interacting with people just not a fan of phonecalls 6/10)</li>
-          <li>Nanny (The kids were so sweet! 10/10)</li>
-          <li>Dog Walker and sitter (Pets are always fun 10/10) </li>
-          <li>Library shelver (Got me back into reading 8/10)</li>
-        </ul>
-      </ul>
+        <li>I have completed over 50 puzzles during quarantine</li>
+       </ul>
+      </a>
+      <img className='quinnImg' src={quinn}/>
+      <h2 className='quinnName'> The newest member of the Family: Quinn 💗 </h2>
       </div>
  );
 }
 
-// make circus animation, thing move in background
-//picture of me
-// move name back and forth
-//art woork
-//picture of pets / click they give description
-//jigsaw puzzle
-//family facts
-//fun fonts
-//fandoms im in
-//maybe api
+// setInterval(
+//   function () {
+//     var randomColor = Math.floor(Math.random()*16777215).toString(16);
+//     document.body.style.backgroundColor = "#"+randomColor;
+//   },1000);
 
-//function FunColors() {
-// for (i = 0; i < 10; i++) {
-//    document.body.style.backgroundColor = 'rgb(' + i*20 + ',' + 0 + ',' + 0 + ')';
- // }
-//}
-
-
-//setInterval(
-  //function () {
-    //var randomColor = Math.floor(Math.random()*16777215).toString(16);
-    //document.body.style.backgroundColor = "#"+randomColor;
-  //},1000);
 
 export default MargaretComponent;
