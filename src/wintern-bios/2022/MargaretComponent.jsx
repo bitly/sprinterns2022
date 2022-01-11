853b4f15502c29cc295bcdab3a1a992e7ce3b6cb
Margaret Diaz <mdiaz@MargaretsMBP16.fios-router.home>
2022-01-11 10:07:46 -0500

    “your-commit-message”

--- src/wintern-bios/2022/MargaretComponent.jsx
+++ src/wintern-bios/2022/MargaretComponent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import img from '../../assets/bio-photos/PROFILE.JPEG';
 import bella from '../../assets/miscellaneous/bella.jpg';
 import broady from '../../assets/miscellaneous/broady.jpg';
@@ -9,6 +10,19 @@ import winnie from '../../assets/miscellaneous/winnie.png';
 import olddoggies from '../../assets/miscellaneous/olddoggies.png';
 import infinity from '../../assets/miscellaneous/infinity.png';
 import quinn from '../../assets/miscellaneous/quinn.png';
+=======
+//var randomColor = Math.floor(Math.random()*16777215).toString(16);
+import img from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/bio-photos/PROFILE.JPEG';
+import bella from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/bella.jpg';
+import broady from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/broady.jpg';
+import jason from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/jason.png';
+import sharon from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/sharon.png';
+import stevie from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/stevie.png';
+import winnie from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/winnie.png';
+import olddoggies from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/olddoggies.png';
+import infinity from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/infinity.png';
+import quinn from '/Users/mdiaz/Desktop/interns/sprinterns2022/src/assets/miscellaneous/quinn.png';
+>>>>>>> fde619e (“your-commit-message”)
 
 const MargaretComponent = () => {
  return (
@@ -45,6 +59,7 @@ const MargaretComponent = () => {
       <a href="https://www.youtube.com/watch?v=kOHB85vDuow"className='twiceButton'>Twice</a>
       <a href="https://www.youtube.com/watch?v=TmKh7lAwnBI"className='badBunny'>Bad Bunny</a>
       <a href="https://www.youtube.com/watch?v=clwLKJ294u4"className='britneyButton'>Britney Spears</a>
+      <a href="https://www.youtube.com/watch?v=n5LWsc-qCdA"className='meganButton'>Megan Thee Stallion</a>
       <a className='favoriteFacts'> Extra </a>
           <h2 className='center'>FAVORITES:</h2>
           <a className='myFavs'>
