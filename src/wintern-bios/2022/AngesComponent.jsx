a6585d7246ffe3bfc56d9eee62dc938697cd510b
Ange Louis <alouis@anges-mbp-16.myfiosgateway.com>
2022-01-10 23:31:30 -0500

    Added a function for an alert and finshed about me section

--- src/wintern-bios/2022/AngesComponent.jsx
+++ src/wintern-bios/2022/AngesComponent.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import img from '../../assets/bio-photos/79405.jpg';
 
-const App = () => {
-    const content = 'If life is so blue, then select another colour from the rainbow';
-
-    const ele = '<span>' + content.split('').join('</span><span>') + '</span>';
-
+function clickMe() {
+    alert('In my defense, I was left unsupervised.');
 }
+
 
 const AngesComponent = () => {
 
@@ -19,21 +17,29 @@ const AngesComponent = () => {
             <div id="scroll-container">
             <div id="scroll-text">&#x1F338; Hi, My name is Ange &#x1F338;</div>
             </div>
-            <img src = {img}/>
+            <img src = {img} />
+            <div className='buton'>
+                <button onClick={clickMe}>Click for an inspirational work quote!</button>
+            </div>
             <div className='contact'>
                <div><h3>CONTACT INFORMATION &#x1F4DE;</h3></div>
-               <div><h4>LinkedIn: linkedin.com/in/ange-louis</h4></div> 
-               <div><h4>Github: github.com/angelouis</h4></div> 
+               <div><h4>LinkedIn: <a href="https://www.linkedin.com/in/ange-louis/" >linkedin.com/in/ange-louis</a></h4></div> 
+               <div><h4>Github: <a href= "https://www.github.com/angelouis">github.com/angelouis</a></h4></div> 
                 
             </div>
             <div className='aboutme'>
              <p class="type">Here is some information about me :) </p>
 
-             <p class="type">Tim Hortons queues too keener to the donair, and its 
-             garburator -- igloo Canuck melted, eh? Maclean's magazine ranked pop first of tuque beer Canada's puck pissed 
-             Tim Hortons hoser.</p>
+             <p class="type">My name is Ange Louis and I am a junior at City College. I am a Computer 
+             Engineering Major. I am from Brookyn, NY. I can speak two languages. I recently got my driver's license
+             so watch out world</p>
 
-             <p class="type">My name is Ange Louis and I am </p>
+             <p class="type">Things That I Like: I love the horror genre. My favorite show is The Walking 
+             Dead. It has prepared me for a zombie apocalypse. I also love watching streamers play video 
+             games</p>
+
+             <p class="type">My Career Goals: My goal is to become a software engineer. Besides that, I would love to 
+             own a business, specifically an ice cream shop.</p>
 
             </div>
             
