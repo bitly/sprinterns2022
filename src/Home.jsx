adbe97ca0e65a7810a74932ae451d4346f34d649
Lauren Avilla <92548296+lauren-avilla@users.noreply.github.com>
2022-01-19 15:36:21 -0500

    yaml ticket (#13)

---
+++ src/Home.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './styles/main.scss';
+import Header from './Header.jsx';
+import Sidebar from './Sidebar.jsx'
+
+function Home({api}) {
+
+const getSidebarData = (api) => {
+  const paths = api.paths;
+  const data = [];
+  for (let route in paths) {
+    const methods = paths[route];
+    for (let methodType in methods) {
+      const method = methods[methodType];
+      const title = method.summary;
+      data.push({
+        title: title,
+        type: methodType,
+        path: route
+      })
+    }
+  }
+  return data;
+}
+
+const sidebarData = getSidebarData(api);
+
+    return (
+        <>
+        <Header />  
+        <Sidebar items = {sidebarData}/>
+    </>
+  );
+        
+ }
+
+export default Home;
