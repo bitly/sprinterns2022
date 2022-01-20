f491f44188997b7b7ab61e63443f488c04fb8f53
Skyler Basco <sbasco@Skylers-MBP-16.home>
2022-01-20 10:56:16 -0500

    rebasing 4

--- src/index.jsx
+++ src/index.jsx
@@ -11,13 +11,8 @@ import Home from './Home.jsx';
 import Team from './Team.jsx';
 import APIinfo from './APIinfo.jsx';
 import SwaggerParser from "@apidevtools/swagger-parser";
-import AngesComponent from './wintern-bios/2022/AngesComponent.jsx';
-import KadiatouComponent from './wintern-bios/2022/KadiatouComponent.jsx';
-import LaurenComponent from './wintern-bios/2022/LaurenComponent.jsx';
-import SkylerComponent from './wintern-bios/2022/SkylerComponent.jsx';
-import MargaretComponent from './wintern-bios/2022/MargaretComponent.jsx';
 import CommentsPage from './CommentsPage.jsx';
-import Header from './Header.jsx';
+
 const App = () => {
 const [api, setApi] = useState(null);
 
@@ -49,27 +44,9 @@ useEffect(() => {
           <Route path="/team-2022">
             <Team year={2022} />
           </Route>
-          <Route path="/ange-louis">
-            <Header />
-            <AngesComponent />
-          </Route>
-          <Route path="/kadiatou-diallo">
-            <Header />
-            <KadiatouComponent />
-          </Route>
-          <Route path="/lauren-avilla">
-            <Header />
-            <LaurenComponent />
-          </Route>
-          <Route path="/skyler-basco">
-            <Header />
-            <SkylerComponent />
-          </Route>
-          <Route path="/margaret-diaz">
-            <Header />
-            <MargaretComponent />
           <Route path="/allComments">
             <CommentsPage />
+          </Route>
           <Route path="/">
               {api ? <Home api={api} /> : <div>Loading ...</div>}
           </Route>
