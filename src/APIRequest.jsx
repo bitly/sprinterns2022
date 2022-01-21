ed90f67259b02a6bef1e43679fc818f42cf9d877
Lauren Avilla <laurenavilla@Laurens-MacBook-Air.local>
2022-01-21 13:29:10 -0500

    updating api request branch

--- src/APIRequest.jsx
+++ src/APIRequest.jsx
@@ -4,11 +4,15 @@ const APIRequest = ({method}) => {
     return <div className = "APIRequest">
         <div className = "APIRequest__title" >{method.title}</div>
         <div className = "APIRequest__endpoint">API Endpoint</div>
+        <div className = "APIRequest__method" >{method.type}</div>
         <div className = "APIRequest__path" >{method.path}</div>
         <div className = "APIRequest__accesstoken">Access Token</div>
+        <form><input className = "APIRequest__accesstokenfield"></input></form>
         <div className = "APIRequest__pathparameters">Path Parameters</div>
         <div className = "APIRequest__rbschema">Request Body Schema</div>
-
+        <button className = "APIRequest__tryme">Try Me!</button>
+        <button className = "APIRequest__reset">Reset</button>
+        <div className = "APIRequest__responsejson">Response JSON</div>
     </div>
 }
 
