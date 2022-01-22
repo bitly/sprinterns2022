9ba237a8a5bedeab3d0d0bd5778a6a920411473b
Margaret Diaz <mdiaz@MargaretsMBP16.fios-router.home>
2022-01-22 00:57:09 -0500

    “your-commit-message”

--- src/Forum.jsx
+++ src/Forum.jsx
@@ -12,16 +12,13 @@ const PostButton = ({buttonText}) => {
 const Comment = ({name,email,date,subject,commentText}) => {
   return (
   <div className="comments">
-  <div className='container'> 
+  <div className='labelContainer'>
+  <div className="date">{date}</div>
   <div className="name">{name}</div>
   <div className="email">{email}</div>
-  <div className="date">{date}</div>
   </div>
-  <div className='containerText'>
   <div className="subject">{subject}</div>
-  <br />
   <div className="commentText">{commentText}</div>
-  </div>
   </div>
   );
 }
@@ -42,13 +39,30 @@ const Forum = () => {
     getComments();
   },[]) 
 
+  
+// const handleRomve = asynch (id) = () => {
+//     try{
+//       const SERVER_URL = ""
+//       const res = await axios.delete(SERVER_URL )
+
+//     }
+  // }
+    // axios 
+      // .delete(SERVER_URL + 'savedetails'/ {data: {id}})
+      // // .post(SERVER_URL + 'savedetails', {f_name: first_name, l_name: last_name, email: email, subject: subject, comment: comment})
+      // .then((res) => {   
+      //   // setResponse(res.data)
+      // })   
+  // } 
+
   return (
     <>
     <Header />
     <div className='webContainer'>
       <div className= "Forum">
-        <h1 className="ForumHeader">Help Center Forum</h1>
+        <h1 className="ForumHeader">Help Center Forum
         <Link to ="/Submit"><PostButton buttonText="Post New Comment"></PostButton></Link> 
+        </h1>
         {comments.map( (commentArray) => {
           return <Comment name = {commentArray[1]+commentArray[2]} email = {commentArray[3]} date ={commentArray[4]} subject = {commentArray[5]} commentText = {commentArray[6]}></Comment> 
         })}
