6a21a59bc075970eae8bb849821d6ec6b67c4bc1
Skyler Basco <58148243+skylerbasco@users.noreply.github.com>
2022-01-25 11:46:47 -0500

    api request commit (#20)

--- src/APIRequest.jsx
+++ src/APIRequest.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import ReactJson from 'react-json-view';
 
 const APIRequest = ({method}) => {
     
@@ -13,7 +15,13 @@ const APIRequest = ({method}) => {
             return 'red'
         }
 }
-    
+        const [pathParams, setPathParams] = useState ({})
+        const [response, setResponse] = useState ({})
+        const [accesstoken, setAccessToken] = useState ("")
+        useEffect(() => {
+            setPathParams({})
+            setResponse({})
+        }, [method])
     return <>
         <div className = "APIRequest leftside">
             <div className = "APIRequest__title" >{method.title}</div>
@@ -25,21 +33,69 @@ const APIRequest = ({method}) => {
             </div>
 
             <div className = "APIRequest__accesstoken">Access Token</div>
-            <div><form><input className = "APIRequest__accesstokenfield"></input></form></div>
+            <div><form><input 
+            className = "APIRequest__accesstokenfield" 
+            onChange={(event) => {
+                setAccessToken(event.target.value)
+            } 
+            }
+            value = {accesstoken}
+            /> 
+            </form></div>
                 
             <div className = "APIRequest__pathparameters">Path Parameters</div>
-                
+
+            {method.parameters?.filter((param) => {
+                return param.in === "path"
+            }).map(parametersObject=> {
+                return <div> 
+                    {parametersObject.name} 
+
+                        <div>
+                            <input 
+                                value={pathParams?.[parametersObject.name] ? pathParams[parametersObject.name] : ""}
+                                placeholder={parametersObject.schema.type} 
+                                onChange={(event) => {
+                                    setPathParams({
+                                        ...pathParams,
+                                        [parametersObject.name]: event.target.value
+                                    })
+                                }
+                            }></input>
+                        </div>
+                    </div>
+            }
+        ) }    
             <div className = "APIRequest__buttonscontainer">
-                <div><button className = "APIRequest__tryme">Try Me!</button></div>
-                <div><button className = "APIRequest__reset">Reset</button></div>
+                <div>
+                <button className = "APIRequest__tryme" onClick = {() => {
+            let requestPath = method.path
+            for (let key in pathParams) {
+                requestPath = requestPath.replace("{" + key + "}", pathParams[key])
+            }
+                axios
+                    .post('api', {apiEndpoint: requestPath, authToken: accesstoken, params: {}, Method: method.type.toUpperCase()})
+                    .then((response) => {
+                        setResponse(response.data)
+                    })
+           
+        }}> Try it! </button></div>
+                <div><button 
+                className = "APIRequest__reset"
+                onClick = {() => {
+                    setPathParams({})
+                    setResponse({}) 
+                }
+                }
+                >Reset</button></div>
+
             </div>
         </div>
 
         <div className = "Response rightside">
             <div className = "Response__json">Response JSON</div>
-            <div className = "Response__testresponse">test response</div>
+            <div className = "Response__testresponse"><ReactJson src={response} theme="bright:inverted"/></div>
         </div>
-
     </>
 }
 
